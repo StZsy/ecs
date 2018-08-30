848f799f209ee15a220422cc7ab1e0850fa006aa
StZsy <ndjlzsy@126.com>
2018-08-31 01:20:29 +0800

    i

--- includes/modules/payment/heepay_wy.php
+++ includes/modules/payment/heepay_wy.php
@@ -97,7 +97,7 @@ class heepay_wy
         $requestTime=date('YmdHis', time());	//请求时间	商户请求接口时间 yyyyMMddhhmmss	
         $version="1.0";	//版本号	商户请求版本号	
         $notifyUrl="http://$_SERVER[HTTP_HOST]/ecs/flow.php?step=done";	//通知URL	
-        $callBackUrl="http://$_SERVER[HTTP_HOST]/ecs/flow.php";	//同步返回URL	本次交易同步返回URL	
+        $callBackUrl="http://www.e7chong.com/mall/";	//同步返回URL	本次交易同步返回URL	
 
         $description="goods_info";	//商品信息	本次交易商品信息	
         $clientIp="192.168.8.103";	//用户ip	发起交易用户的ip	
