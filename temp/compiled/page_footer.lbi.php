92e465074463a53107ffad91c7313e2a6373bc57
st_yuan <ndjlzsy@126.com>
2018-08-31 00:59:20 +0800

    i

--- temp/compiled/page_footer.lbi.php
+++ temp/compiled/page_footer.lbi.php
@@ -67,21 +67,12 @@ if ($this->_foreach['bottom']['total'] > 0):
     <ul id="link_slide">
       <li> 
         <?php if ($this->_var['navigator_list']['bottom']): ?> 
-<<<<<<< HEAD
-        <?php $_from = $this->_var['navigator_list']['bottom']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'nav_0_17452900_1535554434');$this->_foreach['nav_bottom_list'] = array('total' => count($_from), 'iteration' => 0);
+        <?php $_from = $this->_var['navigator_list']['bottom']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'nav_0_03919600_1535604803');$this->_foreach['nav_bottom_list'] = array('total' => count($_from), 'iteration' => 0);
 if ($this->_foreach['nav_bottom_list']['total'] > 0):
-    foreach ($_from AS $this->_var['nav_0_17452900_1535554434']):
+    foreach ($_from AS $this->_var['nav_0_03919600_1535604803']):
         $this->_foreach['nav_bottom_list']['iteration']++;
 ?> 
-        <a href="<?php echo $this->_var['nav_0_17452900_1535554434']['url']; ?>"  > <?php echo $this->_var['nav_0_17452900_1535554434']['name']; ?></a> <?php if (! ($this->_foreach['nav_bottom_list']['iteration'] == $this->_foreach['nav_bottom_list']['total'])): ?><span>|</span><?php endif; ?> 
-=======
-        <?php $_from = $this->_var['navigator_list']['bottom']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'nav_0_37112800_1535569141');$this->_foreach['nav_bottom_list'] = array('total' => count($_from), 'iteration' => 0);
-if ($this->_foreach['nav_bottom_list']['total'] > 0):
-    foreach ($_from AS $this->_var['nav_0_37112800_1535569141']):
-        $this->_foreach['nav_bottom_list']['iteration']++;
-?> 
-        <a href="<?php echo $this->_var['nav_0_37112800_1535569141']['url']; ?>"  > <?php echo $this->_var['nav_0_37112800_1535569141']['name']; ?></a> <?php if (! ($this->_foreach['nav_bottom_list']['iteration'] == $this->_foreach['nav_bottom_list']['total'])): ?><span>|</span><?php endif; ?> 
->>>>>>> 22e3f17c7ab04c2d1ca248e2e58ea026aec0b7bd
+        <a href="<?php echo $this->_var['nav_0_03919600_1535604803']['url']; ?>"  > <?php echo $this->_var['nav_0_03919600_1535604803']['name']; ?></a> <?php if (! ($this->_foreach['nav_bottom_list']['iteration'] == $this->_foreach['nav_bottom_list']['total'])): ?><span>|</span><?php endif; ?> 
         <?php endforeach; endif; unset($_from); ?><?php $this->pop_vars();; ?> 
         <?php endif; ?> 
       </li>
